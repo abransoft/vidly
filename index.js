7b04f39633167b40567c3f16df5880507c8f0a0b
Márcio Abrantes <abransoft@gmail.com>
2020-06-11 16:18:59 -0300

    API POST behavior created

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Joy = require('@hapi/joi');
+const Joi = require('@hapi/joi');
 
 const app = express();
 app.use(express.json());
@@ -23,6 +23,32 @@ app.get('/api/genres/:id', (req, res) => {
 
     res.send(genre);
 });
+
+app.post('/api/genres', (req, res) => {
+    // Validate
+    const { error } = validateGenre(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // Prepare
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+
+    // Add
+    genres.push(genre);
+    res.send(genre);
+});
+
+function validateGenre(genre) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(genre);
+}
 
 
 const port = process.env.PORT || 3000;
