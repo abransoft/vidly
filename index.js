7e24bd71047e62c0805bea2d4abba14dd8f8e22a
Márcio Abrantes <abransoft@gmail.com>
2020-07-14 09:27:56 -0300

    Added movies,rentals models and routes to the API

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const Joi = require('@hapi/joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
 const express = require('express');
 const mongoose = require('mongoose');
+
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
@@ -11,10 +17,12 @@ mongoose.connect('mongodb://localhost/vidly')
     .catch(() => console.log('MongoDB has not been connected'));
 
 const app = express();
-
 app.use(express.json());
+
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
